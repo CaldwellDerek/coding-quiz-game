5fe4c0faf449e0ed5d9f58d076aa9d3a11ca6f81
Derek Caldwell <djohncaldwell@gmail.com>
2023-01-01 18:08:48 -0800

    timer js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,24 @@
 let quizIntro = document.querySelector(".quiz-intro");
 let quizContent = document.querySelector(".quiz-content");
 let startButton = document.querySelector(".start-quiz");
+let timer = document.querySelector(".timer");
+
+// Function that starts the timer
+function countdown() {
+    let timeLeft = 60;
+    timer.textContent = `Timer: ${timeLeft}`;
+    let timeInterval = setInterval( ()=> {
+        timeLeft --;
+        timer.textContent = `Timer: ${timeLeft}`;
+        if (timeLeft == 0){
+            clearInterval(timeInterval);
+            timer.textContent = "Timer: 0";
+        }
+    }, 1000);
+}
 
 startButton.addEventListener("click", ()=> {
-    console.log("it's working!!!")
     quizIntro.setAttribute("style", "display: none;");
     quizContent.setAttribute("style", "display: block;");
+    countdown();
 });
